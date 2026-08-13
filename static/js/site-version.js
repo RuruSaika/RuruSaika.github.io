0ef9875e8c32c19dca7ff6574b0311f6686858fe
RuruSaika <rurusaika.official@gmail.com>
2026-08-13 23:36:31 +0800

    Use accent color for underlines

--- static/js/site-version.js
+++ static/js/site-version.js
@@ -3,7 +3,7 @@
         github: "2026.08.12.4",
         sites: "2026.08.12.4",
     });
-    const releaseNumbers = Object.freeze({ github: 9, sites: 9 });
+    const releaseNumbers = Object.freeze({ github: 10, sites: 10 });
     const target = location.hostname.endsWith("chatgpt.site") ? "sites" : "github";
     const versions = Object.freeze({
         github: `${preferenceVersions.github}.${releaseNumbers.github}`,
